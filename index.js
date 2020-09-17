9ae0b37335a4078b7a220ec9c434ab3dbc2d908d
SushantSangle <sangle_sushant@yahoo.in>
2020-09-17 23:57:47 +0530

    Fix: fixed mongodb dependency crash, feat: docker-compose.yml for deployment on small scale single server

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-// Example express application adding the parse-server module to expose Parse
-// compatible API routes.
-var express = require('express');
-var ParseServer = require('parse-server').ParseServer;
-const config=require('./config');
-const Config=config.Config;
-const SSL=config.SSL;
-
+var express = require("express");
+var ParseServer = require("parse-server").ParseServer;
+const config = require("./config");
+const Config = config.Config;
+const SSL = config.SSL;
+console.log("Setting up parseServer");
 var api = new ParseServer(Config);
 
 var app = express();
-var mountPath =  '/parse';
+var mountPath = "/parse";
 app.use(mountPath, api);
 
-app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+app.get("/", function(req, res) {
+  res
+    .status(200)
+    .send(
+      "I dream of being a website.  Please star the parse-server repo on GitHub!"
+    );
 });
 
-const Server = (Config.serverType=='http')?require('http'):require('https');
+const Server = Config.serverType == "http" ? require("http") : require("https");
 var httpServer;
 
-const fs = require('fs');
-if(Config.serverType=='http'){
+const fs = require("fs");
+if (Config.serverType == "http") {
+  console.log("server Created");
   httpServer = Server.createServer(app);
-}else{
-  httpServer = Server.createServer({
-    key: fs.readFileSync(SSL.key),
-    cert: fs.readFileSync(SSL.cert)
-  },app);
+} else {
+  httpServer = Server.createServer(
+    {
+      key: fs.readFileSync(SSL.key),
+      cert: fs.readFileSync(SSL.cert),
+    },
+    app
+  );
 }
 
 httpServer.listen(Config.port, function() {
-    console.log('parse-server-example running on port ' + Config.port + '.');
+  console.log("parse-server-example running on port " + Config.port + ".");
 });
 
-console.log()
+console.log("starting server...");
 // This will enable the Live Query real-time server
 ParseServer.createLiveQueryServer(httpServer);
